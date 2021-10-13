878109c24b9309844e71994474a26408b9819974
Peter Perov <peterperov@gmail.com>
2021-10-13 18:35:41 +0100

    AAD integration + KeyVault Storage

--- 117-AzureAD-Application/code/ppaad/Controllers/HomeController.cs
+++ 117-AzureAD-Application/code/ppaad/Controllers/HomeController.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApp_OpenIDConnect_DotNet.Models;
+using Microsoft.Extensions.Configuration;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+using Azure.Identity;
 
 namespace WebApp_OpenIDConnect_DotNet.Controllers
 {
@@ -14,10 +18,13 @@ namespace WebApp_OpenIDConnect_DotNet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        // requires using Microsoft.Extensions.Configuration;
+        private readonly IConfiguration Configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public IActionResult Index()
@@ -25,10 +32,6 @@ namespace WebApp_OpenIDConnect_DotNet.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -36,5 +39,55 @@ namespace WebApp_OpenIDConnect_DotNet.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Secret()
+        {
+            ViewData["Message"] = "Your Settings page.";
+
+            var list = new List<KeyValuePair<string, string>>();
+
+            list.Add(new KeyValuePair<string, string>("key1", "value"));
+
+
+
+            list.Add(new KeyValuePair<string, string>("MySecret", GetSecret("mysecret")));
+            list.Add(new KeyValuePair<string, string>("ExamplePassword", Configuration["ExamplePassword"]));
+            list.Add(new KeyValuePair<string, string>("StorageConnectionString", Configuration["StorageConnectionString"]));
+            list.Add(new KeyValuePair<string, string>("testsecret001", Configuration["testsecret001"]));
+
+
+
+            list.Add(new KeyValuePair<string, string>("StorageConnectionString", Configuration["StorageConnectionString"]));
+            list.Add(new KeyValuePair<string, string>("ExamplePassword", Configuration["ExamplePassword"]));
+
+
+            ViewBag.list = list;
+
+
+            return View();
+        }
+
+
+        private string GetSecret(string name)
+        {
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+            };
+            var client = new SecretClient(new Uri("https://ppkeyvault01.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret(name);
+
+            string secretValue = secret.Value;
+
+            return "";
+        }
+
     }
 }
--- 117-AzureAD-Application/code/ppaad/Program.cs
+++ 117-AzureAD-Application/code/ppaad/Program.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Azure.Identity;
 
 namespace WebApp_OpenIDConnect_DotNet
 {
@@ -21,6 +27,43 @@ namespace WebApp_OpenIDConnect_DotNet
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var keyVaultEndpoint = GetKeyVaultEndpoint();
+                    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+                        builder.AddAzureKeyVault(
+                            keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
+            ;
+
+
+        // PP: this is now being included into CreateHostBuilder
+        public static IWebHost BuildWebHost(string[] args) =>
+               WebHost.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((ctx, builder) =>
+                   {
+                       var keyVaultEndpoint = GetKeyVaultEndpoint();
+                       if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                       {
+                           var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                           var keyVaultClient = new KeyVaultClient(
+                               new KeyVaultClient.AuthenticationCallback(
+                                   azureServiceTokenProvider.KeyVaultTokenCallback));
+                           builder.AddAzureKeyVault(
+                               keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                       }
+                   }
+                ).UseStartup<Startup>()
+                 .Build();
+
+        private static string GetKeyVaultEndpoint() => "https://ppkeyvault01.vault.azure.net/";
+
     }
 }
