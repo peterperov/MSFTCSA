75d941a06a902c93f0d2ce333ca59e3b08dc5029
Peter <peterperov@gmail.com>
2022-03-13 17:18:11 +0000

    initial

--- 119-SQL Assessment/SelectFromAllTables.sql
+++ 119-SQL Assessment/SelectFromAllTables.sql
@@ -1,32 +1,55 @@
 /*
 
+Silly load script that selects * from every table in the database
 
+Major source of inspiration: 
+
+https://stackoverflow.com/questions/2875768/how-do-i-list-all-tables-in-all-databases-in-sql-server-in-a-single-result-set
 */
 
+use master
+go
 
 declare @table varchar(250); 
 declare @schema varchar(200); 
-declare @sql varchar(max); 
+declare @database varchar(200);
+declare @sql nvarchar(max); 
+
+SET NOCOUNT ON
+DECLARE @AllTables table (DbName sysname,SchemaName sysname, TableName sysname)
+DECLARE
+     @SearchDb nvarchar(200)
+    ,@SearchSchema nvarchar(200)
+    ,@SearchTable nvarchar(200)
+
+SET @SearchDb='%'
+SET @SearchSchema='%'
+SET @SearchTable='%'
+SET @SQL='select ''?'' as DbName, s.name as SchemaName, t.name as TableName from [?].sys.tables t inner join [?].sys.schemas s on t.schema_id=s.schema_id WHERE ''?'' LIKE '''+@SearchDb+''' AND s.name LIKE '''+@SearchSchema+''' AND t.name LIKE '''+@SearchTable+''''
+
+INSERT INTO @AllTables (DbName, SchemaName, TableName)
+    EXEC sp_msforeachdb @SQL
+SET NOCOUNT OFF
 
 DECLARE cTables INSENSITIVE CURSOR FOR
-	SELECT s.name as schemaName, t.name as tablename 
-	FROM sys.tables t
-	LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
+select dbName, SchemaName, TableName from @AllTables; 
 
 OPEN cTables
 
-FETCH NEXT FROM cTables INTO @schema, @table
+FETCH NEXT FROM cTables INTO @database, @schema, @table
 WHILE (@@fetch_status <> -1)
 BEGIN
     IF (@@fetch_status <> -2)
     BEGIN
-	
-		set @sql = 'SELECT * FROM ' + @schema + '.' + @table; 
 
+		SET @sql = 'SELECT * FROM ' + @database + '.' + @schema + '.' + @table; 
+	
 		print @sql; 
-		
+
+		execute sp_executesql @sql;
+
     END
-    FETCH NEXT FROM cTables INTO @schema, @table
+    FETCH NEXT FROM cTables INTO  @database, @schema, @table
 END
 
 CLOSE cTables
