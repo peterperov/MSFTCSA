fef6fd3bb391a6593e122a4adf564b46a112273a
Peter <peterperov@gmail.com>
2022-03-14 13:57:02 +0000

    121

---
+++ 121-Who can be migrated/WhatCanBeMigrated.ps1
@@ -0,0 +1,11 @@
+﻿
+$filePath = "move-support-resources.csv"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tfitzmac/resource-capabilities/master/move-support-resources.csv" -OutFile $filePath
+
+
+$mytable = Import-CSV -Path $filePath | Group-Object -AsHashTable -Property "Resource"
+
+$mytable["Microsoft.ApiManagement/reportfeedback"]
+
+
