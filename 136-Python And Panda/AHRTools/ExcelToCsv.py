b72dd4d58458811aeb08796a7818460198151051
peter perov <peterperov@gmail.com>
2025-04-10 23:05:52 +0100

    ExcelToCSV

--- 136-Python And Panda/AHRTools/ExcelToCsv.py
+++ 136-Python And Panda/AHRTools/ExcelToCsv.py
@@ -2,28 +2,36 @@
 import pandas as pd
 import re
 import csv
+import os
 
-from ExtractExcelTranspose import  TransposeExcel
 
-search = r'\d{4}-\d{2}'  
+def ExcelToCSV(fileName):
+    df1 = pd.read_excel(fileName, sheet_name = 0)
+    df1.to_csv(fileName + ".1.csv.dmp", index=False)
+
+    df2 = pd.read_excel(fileName, sheet_name = 1)
+    df2.to_csv(fileName + ".2.csv.dmp", index=False)
+
+
+
+def AllExcels(folder): 
+
+    for file in os.listdir(folder):
+        filename = os.fsdecode(file)
+        if filename.endswith(".xlsx"):
+            f = os.path.join(folder, filename)
+            print(f)
+            ExcelToCSV(f)
+            continue    
+
+
 
 print( "Hello world")
 
 folder = "C:/Users/peterperov/OneDrive - Microsoft/Desktop/AHR Reports/"
 folder = "W:/01/"
+folder = "W:/Azure/Reporting/01/"
 
 fileName = "AHR-Report-C2025-04-10-181611.xlsx"
 
-# TransposeExcel(fileName)
-
-# read by default 1st sheet of an excel file
-# "ACRs SL5"
-df1 = pd.read_excel(fileName, sheet_name = 0)
-
-df1.to_csv(fileName + ".1.csv.dmp", index=False)
-
-df2 = pd.read_excel(fileName, sheet_name = 1)
-
-df2.to_csv(fileName + ".2.csv.dmp", index=False)
-
-
+AllExcels( folder )
